fc7850784d28db7af4ac1535ac85017e2643b963
Sean Park <seanpark1107@gmail.com>
2020-07-09 18:49:12 -0500

    typescript linter

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 
 const App = () => {
-  return (
-    <div className="App">
-      Test
-    </div>
-  );
-}
+    return <div className="App">Test</div>;
+};
 
 export default App;
